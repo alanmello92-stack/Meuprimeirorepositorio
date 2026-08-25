2f18c93fdde04e262a2378cf452b7a2909207499
alanmello92-stack <alanmello9.2@gmail.com>
2026-08-25 19:41:53 -0300

    Primeiro commit
    Primeiro commit para verificar a função via github

---
+++ AULA3/fase4.py
@@ -0,0 +1,9 @@
+#variaveis
+idade = int (input("Digite sua idade: "))
+altura = float (input("digite sua altura (utilizar ponto e não virgula): "))
+
+#condições
+if idade >= 10 and altura >= 1.40:
+    print("Pode entrar no Brinquedo Radical!")
+else:
+    print("Não pode entrar nesse brinquedo")
---
+++ AULA3/fase5.py
@@ -0,0 +1,9 @@
+#variaveis
+tem_ingresso = (input("Tem ingresso?)Digite: s/n "))
+aniversariante = (input("É aniversariante? Digite: s/n "))
+
+#Condições
+if tem_ingresso == "s" or aniversariante == "s":
+    print("Entrada liberada!")
+else:
+    print("tem que comprar ingresso.")
